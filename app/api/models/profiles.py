72b556991a59f7fd616280b15c1a7c90e6b1b304
samsonajifowoke <samsonajifowoke@gmail.com>
2026-04-20 17:25:18 +0100

    feat: add database models

---
+++ app/api/models/profiles.py
@@ -0,0 +1,40 @@
+import uuid
+from datetime import datetime, timezone
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import (
+    UUID,
+    Index,
+    Float,
+    Integer,
+    VARCHAR,
+    DateTime,
+    PrimaryKeyConstraint,
+)
+
+
+from app.database.base import Base
+
+
+class Profile(Base):
+    __tablename__ = "profiles"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID)
+    name: Mapped[str] = mapped_column(VARCHAR, unique=True)
+    gender: Mapped[str] = mapped_column(VARCHAR)
+    gender_probability: Mapped[float] = mapped_column(Float)
+    age: Mapped[int] = mapped_column(Integer)
+    age_group: Mapped[str] = mapped_column(VARCHAR)
+    country_id: Mapped[str] = mapped_column(VARCHAR(2))
+    country_name: Mapped[str] = mapped_column(VARCHAR)
+    country_probability: Mapped[float] = mapped_column(Float)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=datetime.now(timezone.utc)
+    )
+
+    __table_args__ = (
+        PrimaryKeyConstraint("id", name="profiles_id_pk"),
+        Index("idx_profiles_name", name),
+        Index("idx_profiles_gender", gender),
+        Index("idx_profiles_age_group", age_group),
+        Index("idx_profiles_country_id", country_id),
+    )
