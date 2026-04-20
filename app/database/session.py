5154a4a0fffc07b49e2cab364c1a58738de53e8f
samsonajifowoke <samsonajifowoke@gmail.com>
2026-04-20 16:19:00 +0100

    chore: add database engine and session

---
+++ app/database/session.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    create_async_engine,
+    AsyncSession,
+    async_sessionmaker,
+)
+
+
+from app.core.config import settings
+
+async_engine: AsyncEngine = create_async_engine(
+    url=settings.ASYNC_DB_URL,
+    pool_size=10,
+    max_overflow=5,
+    pool_timeout=10.0,
+    pool_pre_ping=True,
+)
+
+async_session = async_sessionmaker(
+    bind=async_engine,
+    autocommit=False,
+    autoflush=False,
+    class_=AsyncSession,
+    connect_args={"server_settings": {"timezone": "utc"}},
+)
