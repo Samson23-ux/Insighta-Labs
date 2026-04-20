6ca6f4e134af36614fc02c74380a64cb1c58952f
samsonajifowoke <samsonajifowoke@gmail.com>
2026-04-20 17:35:00 +0100

    feat: update schemas
    Removed sample_size field and added country_name field. Changed the type of gender and age_group fields to string from enum

--- app/api/schemas/profiles.py
+++ app/api/schemas/profiles.py
@@ -1,25 +1,5 @@
 from uuid import UUID
-from enum import Enum
-from typing import Optional
 from pydantic import BaseModel, ConfigDict
-
-
-class GenderEnum(str, Enum):
-    MALE = "male"
-    FEMALE = "female"
-
-
-class AgeGroupEnum(str, Enum):
-    CHILD = "child"         # 0-12
-    ADULT = "adult"         # 13-19
-    SENIOR = "senior"       # 20-59
-    TEENAGER = "teenager"   # 60+
-
-
-class ProfileCreate(BaseModel):
-    name: Optional[str] = None
-
-    model_config = ConfigDict(str_strip_whitespace=True)
 
 
 class Profile(BaseModel):
@@ -27,20 +7,16 @@ class Profile(BaseModel):
 
     id: UUID
     name: str
-    gender: GenderEnum
+    gender: StopAsyncIteration
     gender_probability: float
-    sample_size: int
     age: int
-    age_group: AgeGroupEnum
+    age_group: str
     country_id: str
+    country_name: str
     country_probability: float
     created_at: str
 
 
 class ProfileResponse(BaseModel):
     status: str = "success"
-    data: Profile | list[Profile]
-
-
-class ProfileExist(ProfileResponse):
-    message: str = "Profile already exists"
+    data: list[Profile]
